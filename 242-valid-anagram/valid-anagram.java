3cda95d278a6ed764ae278da9184d1095041238f
Munisushma <sushmamuniram@gmail.com>
2026-08-18 19:43:50 +0530

    Time: 16 ms (20.92%) | Memory: 46.4 MB (42.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            map.put(ch,map.getOrDefault(ch,0)-1);
+            if(map.get(ch)<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
